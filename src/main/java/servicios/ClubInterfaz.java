0b65b905fd57eefa6b58af6973e722e22709ae94
unknown <fernandogbernabe@gmail.com>
2024-11-07 09:07:22 +0100

    Declaraciones de métodos en la interfaz de club

--- src/main/java/servicios/ClubInterfaz.java
+++ src/main/java/servicios/ClubInterfaz.java
@@ -1,5 +1,11 @@
-package servicios;
+ package servicios;
+
+import dto.ClubDto;
 
 public interface ClubInterfaz {
+	
+	public boolean altaClub(long id, String nombre, String email);
+	
+	public void compararClub(ClubDto clubDto, String email);
 
 }
